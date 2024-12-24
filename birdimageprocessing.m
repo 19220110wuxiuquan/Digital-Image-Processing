0cef934c66323bdc783b9a7ad9e294a0bd1d747f
19220110wuxiuquan <2275638648@qq.com>
2024-12-24 16:08:16 +0800

    update

--- birdimageprocessing.m
+++ birdimageprocessing.m
@@ -22,7 +22,7 @@ function varargout = birdimageprocessing(varargin)
 
 % Edit the above text to modify the response to help birdimageprocessing
 
-% Last Modified by GUIDE v2.5 23-Dec-2024 09:03:32
+% Last Modified by GUIDE v2.5 24-Dec-2024 15:20:02
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -1013,3 +1013,45 @@ function pushbutton23_Callback(hObject, eventdata, handles)
     % 更新handles结构体中的特征数据
     handles.hogFeaturesTarget = hog_features;
     guidata(hObject, handles);
+
+
+% --- Executes on button press in pushbutton25.
+function pushbutton25_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton25 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+    % 获取之前存储的图像数据
+    img = handles.imageData;
+
+    % 初始化 BirdClassifier 类的对象
+    birdClassifier = BirdClassifier();
+
+    % 使用 BirdClassifier 对象进行预测
+    predictionResult = birdClassifier.predictBirdType(img);
+
+    % 将预测结果显示在 edit4 编辑框中
+    set(handles.edit4, 'String', predictionResult);
+
+    % 更新图形界面以反映更改
+    guidata(hObject, handles);
+
+function edit4_Callback(hObject, eventdata, handles)
+% hObject    handle to edit4 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit4 as text
+%        str2double(get(hObject,'String')) returns contents of edit4 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit4_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit4 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
